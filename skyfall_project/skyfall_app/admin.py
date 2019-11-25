8537bf62d805de962ee8830cb61a4b37e75f097f
JC <avalojc@gmail.com>
2019-11-25 10:00:42 -0500

    superuser created, server up and running

--- skyfall_project/skyfall_app/admin.py
+++ skyfall_project/skyfall_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import KeplerianElements, CESBody, Planet, NearEarthObjects, LastUpdate
+
+admin.site.register([KeplerianElements, CESBody, Planet, NearEarthObjects, LastUpdate])
