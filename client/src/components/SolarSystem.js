97614dd60685e70e718f567476d5d5c84ec5971d
JC <avalojc@gmail.com>
2019-11-25 14:58:54 -0500

    created components files and added first component to app pg

---
+++ client/src/components/SolarSystem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {    Link  } from "react-router-dom";
+import axios from 'axios'
+
+export class SolarSystem extends Component {
+    state = {
+        
+    }
+
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SolarSystem
