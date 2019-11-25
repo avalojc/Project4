288d8dc90b9a2fda7ed950a63ab55a7a8e347bde
JC <avalojc@gmail.com>
2019-11-25 12:38:24 -0500

    corrected relationship in model.

--- skyfall_project/skyfall_app/models.py
+++ skyfall_project/skyfall_app/models.py
@@ -4,57 +4,61 @@ from django.db import models
 
 
 class KeplerianElements(models.Model):
-    semi_major_axis_diacritic = models.CharField(max_length=100)
-    semi_major_axis_subscript = models.CharField(max_length=100)
-    eccentricity_diacritic = models.CharField(max_length=100)
-    eccentricity_subscript = models.CharField(max_length=100)
-    inclination_diacritic = models.CharField(max_length=100)
-    inclination_subscript = models.CharField(max_length=100)
-    mean_longitude_diacritic = models.CharField(max_length=100)
-    mean_longitude_subscript = models.CharField(max_length=100)
-    longitude_of_perihelion_diacritic = models.CharField(max_length=100)
-    longitude_of_perihelion_subscript = models.CharField(max_length=100)
-    longitude_of_the_ascending_node_diacritic = models.CharField(max_length=100)
-    longitude_of_the_ascending_node_subscript = models.CharField(max_length=100)
-    name = models.CharField(max_length=32)
-    x_pos = models.CharField(max_length=100)
-    y_pos = models.CharField(max_length=100)
-    z_pos = models.CharField(max_length=100)
+    semi_major_axis_diacritic = models.CharField(max_length=100, default='')
+    semi_major_axis_subscript = models.CharField(max_length=100, default='')
+    eccentricity_diacritic = models.CharField(max_length=100, default='')
+    eccentricity_subscript = models.CharField(max_length=100, default='')
+    inclination_diacritic = models.CharField(max_length=100, default='')
+    inclination_subscript = models.CharField(max_length=100, default='')
+    mean_longitude_diacritic = models.CharField(max_length=100, default='')
+    mean_longitude_subscript = models.CharField(max_length=100, default='')
+    longitude_of_perihelion_diacritic = models.CharField(max_length=100, default='')
+    longitude_of_perihelion_subscript = models.CharField(max_length=100, default='')
+    longitude_of_the_ascending_node_diacritic = models.CharField(max_length=100, default='')
+    longitude_of_the_ascending_node_subscript = models.CharField(max_length=100, default='')
+    name = models.CharField(max_length=32, default='')
+    x_pos = models.CharField(max_length=100, default='')
+    y_pos = models.CharField(max_length=100, default='')
+    z_pos = models.CharField(max_length=100, default='')
 
     def __str__(self):
         return self.name
 
 
 class CESBody(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-    velocity = models.CharField(max_length=100)
-    x_pos = models.CharField(max_length=100)
-    y_pos = models.CharField(max_length=100)
-    z_pos = models.CharField(max_length=101)
+    name = models.CharField(max_length=100, default='')
+    description = models.CharField(max_length=250, default='')
+    velocity = models.CharField(max_length=100, default='')
+    x_pos = models.CharField(max_length=100, default='')
+    y_pos = models.CharField(max_length=100, default='')
+    z_pos = models.CharField(max_length=101, default='')
 
     def __str__(self):
         return self.name
 
 
-class Planet(CESBody):
-    def __init__(self, x_pos, y_pos, z_pos):
-        super().__init__(x_pos)
-        super().__init__(y_pos)
-        super().__init__(z_pos)
-        self.gas_or_rocky = models.CharField(max_length=101)
+class Planet(models.Model):
+    CESBodyData = models.ForeignKey(CESBody, on_delete=models.CASCADE, related_name='Planet', default='')
+    name = models.CharField(max_length=100, default='')
+    gas_or_rocky = models.CharField(max_length=101, default='')
+    photo_url = models.TextField(default='')
+
+    def __str__(self):
+        return self.name
 
 
-class NearEarthObjects(CESBody):
-    def __init__(self, x_pos, y_pos, z_pos,):
-        super().__init__(x_pos)
-        super().__init__(y_pos)
-        super().__init__(z_pos)
-        self.orbiting_body = models.CharField(max_length=101)
+class NearEarthObjects(models.Model):
+    CESBodyData = models.ForeignKey(CESBody, on_delete=models.CASCADE, related_name='NearEarthObjects', default='')
+    name = models.CharField(max_length=100, default='')
+    orbiting_body = models.CharField(max_length=101, default='')
+    photo_url = models.TextField(default='')
+
+    def __str__(self):
+        return self.name
 
 
 class LastUpdate(models.Model):
-    date = models.CharField(max_length=32)
+    date = models.CharField(max_length=32, default='')
 
     def __str__(self):
         return self.date
