9748870a5aa8a9322731401d0ff1c48efc07ae74
JC <avalojc@gmail.com>
2019-12-02 11:07:11 -0500

    bug with loop not true bug. added comments to console log what action was occuring.

--- client/src/components/KepItem.js
+++ client/src/components/KepItem.js
@@ -12,30 +12,30 @@ export class KepItem extends Component {
         this.kepMathVars()
 
     }
-    refreshSolarSystem0=(kepId)=> {
+    refreshSolarSystem0 = (kepId) => {
         axios.get(`/api/v1/KeplerianElements/${this.props.kepId}`, kepId) /////////need to make it id specific
-        .then((res)=> {
-            this.setState({
-                KeplerianElementsList: res.data
+            .then((res) => {
+                this.setState({
+                    KeplerianElementsList: res.data
+                })
             })
-        })
     }
 
 
 
-    kepMathVars=()=>{
+    kepMathVars = () => {
         //set the givens
         let axd = parseFloat(this.props.axd)
         let axs = parseFloat(this.props.axs)
-        let ecd = parseFloat(this.props.ecd )
-        let ecs = parseFloat(this.props.ecs )
-        let ind = parseFloat(this.props.ind )
-        let ins = parseFloat(this.props.ins )
-        let lgd = parseFloat(this.props.lgd )
-        let lgs = parseFloat(this.props.lgs )
-        let phd = parseFloat(this.props.phd )
-        let phs = parseFloat(this.props.phs )
-        let and = parseFloat(this.props.and )
+        let ecd = parseFloat(this.props.ecd)
+        let ecs = parseFloat(this.props.ecs)
+        let ind = parseFloat(this.props.ind)
+        let ins = parseFloat(this.props.ins)
+        let lgd = parseFloat(this.props.lgd)
+        let lgs = parseFloat(this.props.lgs)
+        let phd = parseFloat(this.props.phd)
+        let phs = parseFloat(this.props.phs)
+        let and = parseFloat(this.props.and)
         let ans = parseFloat(this.props.ans)
         //whats the julian date
         let Teph = 2458814.0328125
@@ -48,32 +48,34 @@ export class KepItem extends Component {
         let phv = phs + phd * Tval                          //deg
         let anv = ans + and * Tval                          //deg
         let uuv = phv - anv
-        let capM = (lgs+(lgd*Tval)) - (phs + (phd * Tval))
-        let ModCapM = capM%360                              //this is in degrees
+        let capM = (lgs + (lgd * Tval)) - (phs + (phd * Tval))
+        let ModCapM = capM % 360                              //this is in degrees
         let ecvDegree = ecv * 180 / Math.PI                 //this is in degrees
-        // let i 
-        // let valE
-        // for (i=0; i< 5; i++){
-        //     // let valE0 = ModCapM + ecvDegree * Math.sin(ModCapM) * (1+ ecv * Math.cos(ModCapM))
-        //     // let valE1 = valE0 - (valE0 - ecvDegree * Math.sin(valE0)-ModCapM)/(1-ecv* Math.cos(valE0))
-        //     // let valMath = valE1-valE0
-        //     // console.log('test')
-        //     // if (valMath<Math.abs(.05)){console.log(i)}
-        //     console.log("this is loop"+i+"of 5")
-        // }
-        // let ModCapMR = ModCapM * Math.PI / 180              //this is in radians
-        // // solve for E make this loop
-        //compute x and y coordinates
-        // let xdirection = 1
-        // //axv * (Math.cos(valE)-ecv)
-        // let ydirection = 1 
-        // //axv * (Math.sqrt(1-ecv*ecv)) * (Math.sin(valE))
+        let i
+        let valE
+        for (i = 0; i <= 10; i++) {
+            if (i === 1) { break }
+            // let valE0 = ModCapM + ecvDegree * Math.sin(ModCapM) * (1 + ecv * Math.cos(ModCapM))
+            // let valE1 = valE0 - (valE0 - ecvDegree * Math.sin(valE0) - ModCapM) / (1 - ecv * Math.cos(valE0))
+            // let valMath = valE1 - valE0
+            // if (valMath < Math.abs(.05)) { console.log(i) }
+            console.log("this is loop " + i +  " of 3 for " + this.props.name)
+        }
 
-        // .then.setState({
-        //     set_x: xdirection, 
-        //     set_y: ydirection,
-        // })
     }
+    // let ModCapMR = ModCapM * Math.PI / 180              //this is in radians
+    // // solve for E make this loop
+    //compute x and y coordinates
+    // let xdirection = 1
+    // //axv * (Math.cos(valE)-ecv)
+    // let ydirection = 1 
+    // //axv * (Math.sqrt(1-ecv*ecv)) * (Math.sin(valE))
+
+    // .then.setState({
+    //     set_x: xdirection, 
+    //     set_y: ydirection,
+    // })
+    // }
 
 
     render() {
